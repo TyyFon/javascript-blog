8572e8aaeca14dcb1d931b9a10aed7e77dcfa2ff
Grzegorz Wasik <wasikgrzegorz3@gmail.com>
2021-10-07 11:36:37 +0200

    Add in /scripts.js/ function removing class _active_ from articles in class .posts

--- js/script.js
+++ js/script.js
@@ -6,10 +6,19 @@
 const titleClickHandler = function(event){
     console.log(event);
 /* remove class 'active' from all article links  */
+const activeLinks = document.querySelectorAll('.titles a.active');
 
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts .post.active');
+
+for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+}
 
   /* get 'href' attribute from the clicked link */
 
@@ -22,4 +31,4 @@ const titleClickHandler = function(event){
   
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
-  }
+  } 
